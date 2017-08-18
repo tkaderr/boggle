4131e082df81b38807109819ac6f99c7edf53f43
Tahim Kader <tahim.kader@gmail.com>
2017-08-17 23:40:17 -0700

    added build function

--- boggle.js
+++ boggle.js
@@ -7,34 +7,42 @@ var sum = 0;
 // for knowing legal moves
 var prevcube;
 
-//boggle dice setup
-var cube1 = new Array('A', 'A', 'A', 'F', 'R', 'S');
-var cube2 = new Array('A', 'A', 'E', 'E', 'E', 'E');
-var cube3 = new Array('A', 'A', 'F', 'I', 'R', 'S');
-var cube4 = new Array('A', 'D', 'E', 'N', 'N', 'N');
-var cube5 = new Array('A', 'E', 'E', 'E', 'E', 'M');
-var cube6 = new Array('A', 'E', 'E', 'G', 'M', 'U');
-var cube7 = new Array('A', 'E', 'G', 'M', 'N', 'N');
-var cube8 = new Array('A', 'F', 'I', 'R', 'S', 'Y');
-var cube9 = new Array('A', 'E', 'G', 'M', 'N', 'N');
-var cube10 = new Array('B', 'J', 'K', 'Q', 'X', 'Z');
-var cube11 = new Array('C', 'C', 'E', 'N', 'S', 'T');
-var cube12 = new Array('C', 'E', 'I', 'I', 'L', 'T');
-var cube13 = new Array('C', 'E', 'I', 'L', 'P', 'T');
-var cube14 = new Array('C', 'E', 'I', 'P', 'S', 'T');
-var cube15 = new Array('D', 'D', 'H', 'N', 'O', 'T');
-var cube16 = new Array('D', 'H', 'H', 'L', 'O', 'R');
-var cube17 = new Array('D', 'H', 'L', 'N', 'O', 'R');
-var cube18 = new Array('E', 'I', 'I', 'I', 'T', 'T');
-var cube19 = new Array('E', 'M', 'O', 'T', 'T', 'T');
-var cube20 = new Array('E', 'N', 'S', 'S', 'S', 'U');
-var cube21 = new Array('F', 'I', 'P', 'R', 'S', 'Y');
-var cube22 = new Array('G', 'O', 'R', 'R', 'V', 'W');
-var cube23 = new Array('I', 'P', 'R', 'R', 'R', 'Y');
-var cube24 = new Array('N', 'O', 'O', 'T', 'U', 'W');
-var cube25 = new Array('O', 'O', 'O', 'T', 'T', 'U');
+//Current boggle dice letter for the current game
+var boggle_curr = new Array();
 
-var grid = new Array(cube1, cube2, cube3, cube4, cube5, cube6, cube7, cube8, cube9, cube10, cube11, cube12, cube13, cube14, cube15, cube16, cube17, cube18, cube19, cube20, cube21, cube22, cube23, cube24, cube25);
+//Store the index of the current word
+var curr_word = new Array();
+var curr_index;
+var prev_index;
+
+//boggle dice setup
+var cube0 = new Array('A', 'A', 'A', 'F', 'R', 'S');
+var cube1 = new Array('A', 'A', 'E', 'E', 'E', 'E');
+var cube2 = new Array('A', 'A', 'F', 'I', 'R', 'S');
+var cube3 = new Array('A', 'D', 'E', 'N', 'N', 'N');
+var cube4 = new Array('A', 'E', 'E', 'E', 'E', 'M');
+var cube5 = new Array('A', 'E', 'E', 'G', 'M', 'U');
+var cube6 = new Array('A', 'E', 'G', 'M', 'N', 'N');
+var cube7 = new Array('A', 'F', 'I', 'R', 'S', 'Y');
+var cube8 = new Array('A', 'E', 'G', 'M', 'N', 'N');
+var cube9 = new Array('B', 'J', 'K', 'Q', 'X', 'Z');
+var cube10 = new Array('C', 'C', 'E', 'N', 'S', 'T');
+var cube11 = new Array('C', 'E', 'I', 'I', 'L', 'T');
+var cube12 = new Array('C', 'E', 'I', 'L', 'P', 'T');
+var cube13 = new Array('C', 'E', 'I', 'P', 'S', 'T');
+var cube14 = new Array('D', 'D', 'H', 'N', 'O', 'T');
+var cube15 = new Array('D', 'H', 'H', 'L', 'O', 'R');
+var cube16 = new Array('D', 'H', 'L', 'N', 'O', 'R');
+var cube17 = new Array('E', 'I', 'I', 'I', 'T', 'T');
+var cube18 = new Array('E', 'M', 'O', 'T', 'T', 'T');
+var cube19 = new Array('E', 'N', 'S', 'S', 'S', 'U');
+var cube20 = new Array('F', 'I', 'P', 'R', 'S', 'Y');
+var cube21 = new Array('G', 'O', 'R', 'R', 'V', 'W');
+var cube22 = new Array('I', 'P', 'R', 'R', 'R', 'Y');
+var cube23 = new Array('N', 'O', 'O', 'T', 'U', 'W');
+var cube24 = new Array('O', 'O', 'O', 'T', 'T', 'U');
+
+var grid = new Array(cube0, cube1, cube2, cube3, cube4, cube5, cube6, cube7, cube8, cube9, cube10, cube11, cube12, cube13, cube14, cube15, cube16, cube17, cube18, cube19, cube20, cube21, cube22, cube23, cube24);
 
 // returns a random letter from the cube
 function chooseLetter(cube){
@@ -61,47 +69,73 @@ Array.prototype.randomize = function(){
 function shuffleBoggleGrid(){
   grid.randomize();
 	var stringOfI;
-	for (var i = 1; i < grid.length+1; i++){
+	for (var i = 0; i < grid.length; i++){
 		stringOfI = "cube"+i.toString();
-		var letter = chooseLetter(grid[i-1]);
+		var letter = chooseLetter(grid[i]);
 		if (letter == 'Q'){letter = "Qu";}
+    boggle_curr.push(letter);
 		document.getElementById(stringOfI).textContent = letter;
 	}
+  console.log(boggle_curr);
 }
 
-// called when mouse press over unactivated cube --> start of a word
+// build the word 
 function buildWord(event){
 	var cube = event.target;
-	if (cube.style.backgroundColor != "#ACCEEC"){
-		cube.style.backgroundColor = "#ACCEEC";
-		currentString = currentString.concat(cube.textContent);
-		console.log(currentString);
-		prevcube = Number(cube.id.substr(4, cube.id.length-1));
-		if (mousedown == 0){mousedown++;}
-	}
+  curr_index = Number(cube.id.slice(4));
+  prev_index = curr_word[curr_word.length-1];
+  console.log("The current word array:", curr_word);
+  console.log("The current index is:",curr_index);
+  console.log("The prev index is:",prev_index);
+
+  if(curr_word.length == 0){
+    cube.style.backgroundColor = "#ACCEEC";
+    prev_index = Number(cube.id.slice(4));
+    curr_word.push(prev_index);
+    currentString = currentString.concat(cube.textContent);
+    console.log("I am in the first check");
+    console.log("The current word array:", curr_word);
+    console.log("The current string is:",currentString);
+    return;
+  }
+  else if(curr_index == prev_index){
+    cube.style.backgroundColor = "white";
+    if(curr_word.length>2){
+      console.log("I am in the second a check");
+      prev_index = curr_word[curr_word.length-2];
+    }
+    curr_word.pop();
+    currentString = currentString.slice(0,-1);
+    console.log("I am in the second b check");
+    console.log("The current word array:", curr_word);
+    console.log("The current string is:",currentString);
+    return;
+  }
+
+  for (var x = 0; x<curr_word.length; x++){
+    if(curr_word[x] == curr_index){
+      console.log("I am in the third check");
+      console.log("The current string is:",currentString);
+      return;
+    }
+  }
+
+
+  if( curr_index == prev_index - 6 || curr_index == prev_index - 5 || curr_index == prev_index - 4 || curr_index == prev_index - 1 || curr_index == prev_index + 1 || curr_index == prev_index + 4 || curr_index == prev_index + 5 || curr_index == prev_index + 6){
+    if (cube.style.backgroundColor != "#ACCEEC"){
+  		cube.style.backgroundColor = "#ACCEEC";
+      prev_index = Number(cube.id.slice(4));
+      curr_word.push(prev_index);
+  		currentString = currentString.concat(cube.textContent);
+      console.log("I am in the fourth check");
+      console.log("The current word array:", curr_word);
+      console.log("The current string is:",currentString);
+      return;
+  	}
+  }
 }
 
-// while in the process of building a word
-function buildingWord(event){
-	if (mousedown == 1){
-		var cube = event.target;
-		if (cube.style.backgroundColor != "orange"){
-			var currentcube = Number(cube.id.substr(4, cube.id.length-1));
-			//if next cube touched is not immediately by last cube, don't do anything
-			if (currentcube < prevcube-5 || currentcube > prevcube+5 ||
-				currentcube == prevcube-2 || currentcube == prevcube+2 ||
-				(prevcube%4 == 0 && currentcube == prevcube-3)||
-				((prevcube-1)%4 == 0 && currentcube == prevcube+3)){
-				return;
-			}
-			cube.style.backgroundColor = "orange";
-			currentString = currentString.concat(cube.textContent);
-			prevcube = currentcube;
-			console.log(currentString);
-		if (mousedown == 0){mousedown=1;}
-		}
-	}
-}
+
 
 function submitWord(event){
 	if (mousedown == 1){
